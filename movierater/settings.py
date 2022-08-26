0a879eab4e6235379cfcca07b3b0119ec4630030
perez1187 <o.perez1187@gmail.com>
2022-08-26 11:30:51 +0200

    migrate api and rest_framework

--- movierater/settings.py
+++ movierater/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'api',
+
+    # 3rd parties
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
